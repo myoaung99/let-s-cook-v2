d89428e6f6ff09956f6c8fcfa1c077806cd4850e
myoaung99 <myomyintaung064@gmail.com>
2023-09-02 19:04:31 +0630

    remove next-font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import {Counter} from '@/features/counter';
-import {Inter} from 'next/font/google';
 import {MealPreviewList} from "@/features/MealPreview/MealPreviewList";
-
-const inter = Inter({subsets: ['latin']});
 
 export default function Home() {
     return (
