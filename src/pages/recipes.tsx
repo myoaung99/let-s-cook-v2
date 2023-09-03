b7bb03fbfb142c80d2c6152c88bfe82b735b89ba
myoaung99 <myomyintaung064@gmail.com>
2023-09-03 10:04:18 +0630

    change ssg fetching

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
-import {wrapper} from "@/app/store";
-import {getRunningQueriesThunk} from "@/services/controller";
-import {RecipeList} from "@/features/Recipes";
-import {getCategories, getMealsByCategory} from "@/features/Recipes/recipesService";
+import { wrapper } from '@/app/store';
+import { getRunningQueriesThunk } from '@/services/controller';
+import { RecipeList } from '@/features/Recipes';
+import {
+    getCategories,
+    getMealsByCategory,
+} from '@/features/Recipes/recipesService';
 
 const Recipes = () => {
     return (
         <>
-            <RecipeList/>
+            <RecipeList />
         </>
     );
-}
+};
 
 export default Recipes;
 
 export const getStaticProps = wrapper.getStaticProps(
     (store) => async (context) => {
         const value = context.params?.value;
-        store.dispatch(getCategories.initiate(''))
-        store.dispatch(getMealsByCategory.initiate(value))
+        store.dispatch(getCategories.initiate(''));
+        if (typeof value === 'string') {
+            store.dispatch(getMealsByCategory.initiate(value));
+        }
         await Promise.all(store.dispatch(getRunningQueriesThunk()));
 
         return {
             props: {},
-            revalidate: 24 * 60 * 60 * 30
+            revalidate: 24 * 60 * 60 * 30,
         };
     }
 );
