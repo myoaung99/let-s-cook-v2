15c7ca979c5759ac8462b2ecdf9b332506316dfd
myoaung99 <myomyintaung064@gmail.com>
2023-09-05 19:17:16 +0630

    hide view all btn at meal preview

--- src/features/MealPreview/MealPreviewList.tsx
+++ src/features/MealPreview/MealPreviewList.tsx
@@ -13,7 +13,7 @@ export const MealPreviewList = ()=> {
                     <h4 className="text-xl mb-5 lg:pl-10 xl:pl-0">
                         Today's Popular Meals
                     </h4>
-                    <Button variant='secondary'>View All</Button>
+                    {/*<Button variant='secondary'>View All</Button>*/}
                 </div>
                 <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                     <PopularMeals />
