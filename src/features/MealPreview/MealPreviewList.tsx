50267c79299b800fc938058cc3f5cb659fe79dd1
myoaung99 <myomyintaung064@gmail.com>
2023-09-05 20:27:55 +0630

    change h4 to p at home->meal preview list

--- src/features/MealPreview/MealPreviewList.tsx
+++ src/features/MealPreview/MealPreviewList.tsx
@@ -10,9 +10,9 @@ export const MealPreviewList = ()=> {
     return (
         <section className='mt-6'>
                 <div className='flex justify-between items-center mb-2'>
-                    <h4 className="text-xl mb-5 lg:pl-10 xl:pl-0">
+                    <p className="text-xl mb-5 lg:pl-10 xl:pl-0">
                         Today's Popular Meals
-                    </h4>
+                    </p>
                     {/*<Button variant='secondary'>View All</Button>*/}
                 </div>
                 <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
