dbdc7ebd73a89ab9d344abcda91cd588f33a70b9
myoaung99 <myomyintaung064@gmail.com>
2023-09-07 07:33:54 +0630

    added: seo tags for meal detail page

--- src/pages/recipes/[recipeId].tsx
+++ src/pages/recipes/[recipeId].tsx
@@ -1,10 +1,54 @@
 import {wrapper} from "@/app/store";
 import {getRunningQueriesThunk} from "@/services/controller";
-import {getMealById} from "@/features/Recipes/recipesService";
+import {getMealById, useGetMealByIdQuery} from "@/features/Recipes/recipesService";
 import {MealDetail} from "@/features/MealDetail";
+import {NextSeo} from "next-seo";
+import React from "react";
+import {skipToken} from "@reduxjs/toolkit/query";
+import {useRouter} from "next/router";
 
 const RecipeDetail = () => {
+    const router = useRouter()
+    const recipeId = router.query?.recipeId;
+
+    const {data} = useGetMealByIdQuery(
+        typeof recipeId === "string" ? recipeId : skipToken,
+        {skip: router.isFallback})
+
+    const mealDetailData = data?.meals[0]
+
     return <>
+        <NextSeo
+            title={mealDetailData?.strMeal}
+            description={`${mealDetailData?.strArea}, ${mealDetailData?.strCategory}, ${mealDetailData?.strArea}`}
+            canonical="https://lets-cook-v2.vercel.app/recipes/*"
+            openGraph={{
+                url: 'https://lets-cook-v2.vercel.app/recipes/*',
+                title: 'Get Recipes',
+                description: 'This is a recipe finder website built by Next.js - Open Graph Description',
+                images: [
+                    {
+                        url: 'https://lets-cook-v2.vercel.app/static/home-preview-800-600.png',
+                        width: 800,
+                        height: 600,
+                        alt: 'Let\'s Cook page - Og Image Alt',
+                    },
+                    {
+                        url: 'https://lets-cook-v2.vercel.app/static/home-preview-900-800.png',
+                        width: 900,
+                        height: 800,
+                        alt: 'Let\'s Cook page - Og Image Alt',
+                    },
+                    {url: 'https://lets-cook-v2.vercel.app/static/home-preview.png'},
+                ],
+            }}
+            additionalMetaTags={[
+                {
+                    name: 'keywords',
+                    content: mealDetailData?.strTags
+                },
+            ]}
+        />
         <MealDetail/>
     </>
 }
@@ -21,7 +65,7 @@ export const getStaticPaths = async () => {
 export const getStaticProps = wrapper.getStaticProps(
     (store) => async (context) => {
         const recipeId = context.params?.recipeId;
-        if(typeof recipeId === 'string'){
+        if (typeof recipeId === 'string') {
             store.dispatch(getMealById.initiate(recipeId))
         }
         await Promise.all(store.dispatch(getRunningQueriesThunk()));
