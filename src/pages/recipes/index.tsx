0f3524ac4aa29f7b8bd786295f3743153102322d
myoaung99 <myomyintaung064@gmail.com>
2023-09-07 07:29:14 +0630

    added: seo tags for recipes page

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -7,11 +7,43 @@ import {
     getCategories,
     getMealsByCategory, getMealsByCountry, getMealsByIngredient,
 } from '@/features/Recipes/recipesService';
+import {NextSeo} from "next-seo";
 
 const Recipes = () => {
     return (
         <section className="mb-16">
-            <RecipeList />
+            <NextSeo
+                title="Find recipes"
+                description="Explore a world of recipes on our Next.js site. From savory dishes to sweet treats, find culinary inspiration here!"
+                canonical="https://lets-cook-v2.vercel.app/recipes"
+                openGraph={{
+                    url: 'https://lets-cook-v2.vercel.app/recipes',
+                    title: 'Get Recipes',
+                    description: 'This is a recipe finder website built by Next.js - Open Graph Description',
+                    images: [
+                        {
+                            url: 'https://lets-cook-v2.vercel.app/static/home-preview-800-600.png',
+                            width: 800,
+                            height: 600,
+                            alt: 'Let\'s Cook page - Og Image Alt',
+                        },
+                        {
+                            url: 'https://lets-cook-v2.vercel.app/static/home-preview-900-800.png',
+                            width: 900,
+                            height: 800,
+                            alt: 'Let\'s Cook page - Og Image Alt',
+                        },
+                        {url: 'https://lets-cook-v2.vercel.app/static/home-preview.png'},
+                    ],
+                }}
+                additionalMetaTags={[
+                    {
+                        name: 'keywords',
+                        content:
+                            'aung, browse, cake, cheesecake, chicken, cook, crafted, creating, culinary, delightful, designed, developed, discover, dishes, embark, expertly, finest, flavors, fried, general, home, honey, join, journey, kentucky, madeira, meals, memories, menu, myint, popular, recipes, reserved, rights, savoring, secrets, tastes, today, unforgettable, view, world, yogurt',
+                    },
+                ]}
+            />            <RecipeList />
         </section>
     );
 };
