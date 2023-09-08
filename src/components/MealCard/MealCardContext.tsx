4663723374f358a9f9c0825812cf66a010605ab0
Myo Myint Aung <myomyintaung064@gmail.com>
2023-09-08 08:28:23 +0630

    modified: typo

--- src/components/MealCard/MealCardContext.tsx
+++ src/components/MealCard/MealCardContext.tsx
@@ -1,4 +1,4 @@
-import {createContext, ReactNode, useContext} from "react";
+import React, {createContext, FC, ReactNode, useContext} from "react";
 import {SuggestionMeal} from "@/types";
 
 interface MealCardProviderProps {
@@ -9,15 +9,15 @@ interface MealCardProviderProps {
 const MealCardContext = createContext<SuggestionMeal | null>(null)
 MealCardContext.displayName = 'MealCardContext'
 
-export const useMealCard = ()=>{
+export const useMealCard = () => {
     const context = useContext(MealCardContext)
-    if(!context){
+    if (!context) {
         throw new Error('MealCardContext* component can only be used within MealCardProvider')
     }
     return context
 }
 
-const MealCardProvider:React.FC<MealCardProviderProps> = ({value, children})=>{
+const MealCardProvider: FC<MealCardProviderProps> = ({value, children}) => {
     return <MealCardContext.Provider value={value}>
         {children}
     </MealCardContext.Provider>
