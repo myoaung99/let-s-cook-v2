3a03cc4d10d68658f2b12955d492e282621f97a0
Myo Myint Aung <myomyintaung064@gmail.com>
2023-09-13 09:05:43 +0630

    hide parallax hero text at mobile

--- src/features/Hero/hero.tsx
+++ src/features/Hero/hero.tsx
@@ -28,7 +28,7 @@ export const Hero = () => {
             <motion.section
                 id='hero'
                 style={{y: textY}}
-                className="relative z-10 flex flex-col md:items-center justify-center h-full space-y-4 px-4 md:px-0">
+                className="hidden lg:flex relative z-10 flex-col md:items-center justify-center h-full space-y-4 px-4 md:px-0">
                 <h2 className="text-slate-900 font-extrabold text-3xl lg:text-5xl tracking-tight md:text-center mb-2 md:mb-0">
                     Embark on a Culinary Journey.&trade;
                 </h2>
@@ -41,6 +41,21 @@ export const Hero = () => {
                     </Button>
                 </div>
             </motion.section>
+            <section
+                id='hero'
+                className="lg:hidden relative z-10 flex flex-col md:items-center justify-center h-full space-y-4 px-4 md:px-0">
+                <h2 className="text-slate-900 font-extrabold text-3xl lg:text-5xl tracking-tight md:text-center mb-2 md:mb-0">
+                    Embark on a Culinary Journey.&trade;
+                </h2>
+                <p className="md:w-3/5 md:text-center text-slate-600">
+                    {HeroDescription}
+                </p>
+                <div className="pt-2 md:pt-4">
+                    <Button size="sm" onClick={handleOnBrowse}>
+                        Browse Recipes
+                    </Button>
+                </div>
+            </section>
 
             <motion.div
                 style={{y: backgroundY}}
