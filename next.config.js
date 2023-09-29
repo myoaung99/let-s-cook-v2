14ada7f105ce63700a277dc7ad076cc739f4e9eb
Myo Myint Aung <myomyintaung064@gmail.com>
2023-09-29 11:45:39 +0630

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,37 +1,49 @@
 /** @type {import('next').NextConfig} */
 // @ts-check
 const withOffline = require('next-offline')
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['www.themealdb.com', 'www.themealdb.com/images/media/meals', 'img.clerk.com']
-    },
-    workboxOpts: {
-        swDest: process.env.NEXT_EXPORT
-            ? 'service-worker.js'
-            : 'static/service-worker.js',
-        runtimeCaching: [
-            {
-                urlPattern: /^https?.*/,
-                handler: 'NetworkFirst',
-                options: {
-                    cacheName: 'offlineCache',
-                    expiration: {
-                        maxEntries: 200,
+module.exports = (phase, { defaultConfig }) => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+    const apiUrl = isDev
+        ? process.env.DEV_API_ROUTE
+        : process.env.PRODUCTION_API_ROUTE;
+
+    const env = {
+       API_URL: apiUrl,
+    };
+
+    return {
+        reactStrictMode: true,
+        env,
+        images: {
+            domains: ['www.themealdb.com', 'www.themealdb.com/images/media/meals', 'img.clerk.com']
+        },
+        workboxOpts: {
+            swDest: process.env.NEXT_EXPORT
+                ? 'service-worker.js'
+                : 'static/service-worker.js',
+            runtimeCaching: [
+                {
+                    urlPattern: /^https?.*/,
+                    handler: 'NetworkFirst',
+                    options: {
+                        cacheName: 'offlineCache',
+                        expiration: {
+                            maxEntries: 200,
+                        },
                     },
                 },
-            },
-        ],
-    },
-    async rewrites() {
-        return [
-            {
-                source: '/service-worker.js',
-                destination: '/_next/static/service-worker.js',
-            },
-        ]
-    },
+            ],
+        },
+        async rewrites() {
+            return [
+                {
+                    source: '/service-worker.js',
+                    destination: '/_next/static/service-worker.js',
+                },
+            ]
+        },
+    };
 };
 
-module.exports = nextConfig;
