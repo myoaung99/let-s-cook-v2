7a9dc91ff1e0e726e172665c0daa85cb8a36baf6
Myo Myint Aung <myomyintaung064@gmail.com>
2023-11-01 11:33:05 +0630

    updated db connection

--- src/lib/db-config.js
+++ src/lib/db-config.js
@@ -1,24 +1,9 @@
 import mongoose from 'mongoose';
 
-const connection = {}
 async function connectDB() {
-    //*================CHECK CONDITIONS======================
-    if (connection.isConnected) {
-        console.log("already connected");
-        return;
-    }
-    if (mongoose.connections.length > 0) {
-        connection.isConnected = mongoose.connections[0].readyState;
-        if (connection.isConnected === 1) {
-            console.log("use previous connection");
-            return;
-        }
-        await mongoose.disconnect();
-    }
-    //*=================NORMAL PROCESS=======================
-    const db = await mongoose.connect(process.env.MONGODB_URI);
-    console.log("new connection");
-    connection.isConnected = db.connections[0].readyState;
+    // Use connect method to connect to the server
+    console.log('Connected successfully to server');
+    return await mongoose.connect(process.env.MONGODB_URI);
 }
 
 export default connectDB;
--- src/pages/api/bookmark.ts
+++ src/pages/api/bookmark.ts
@@ -1,7 +1,7 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
-import connectDB from "@/lib/db-config";
-import User from "@/Model/User";
-import {disconnect} from "mongoose";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import connectDB from '@/lib/db-config';
+import User from '@/Model/User';
+import { disconnect } from 'mongoose';
 
 export default async function handler(
     req: NextApiRequest,
@@ -9,30 +9,31 @@ export default async function handler(
 ) {
     if (req.method === 'POST') {
         const payload = JSON.stringify(req.body);
-        const data = JSON.parse(payload)
+        const data = JSON.parse(payload);
 
-        const recipe = data.recipe
-        const status = data.status
+        const recipe = data.recipe;
+        const status = data.status;
         if (data.userId) {
             await connectDB();
 
-            const user = await User.findOne({clerk_id: data.userId})
+            const user = await User.findOne({ clerk_id: data.userId });
 
             if (user) {
                 const bookmarks = user.bookmarks;
                 let updatedBookmark;
                 if (status === 'ADD_BOOKMARK') {
-                    updatedBookmark = [...bookmarks, recipe]
+                    updatedBookmark = [...bookmarks, recipe];
                 } else if (status === 'REMOVE_BOOKMARK') {
-                    updatedBookmark = bookmarks.filter((bookmark: any) => bookmark.id !== recipe.id)
+                    updatedBookmark = bookmarks.filter(
+                        (bookmark: any) => bookmark.id !== recipe.id
+                    );
                 }
-                const update = {$set: {bookmarks: updatedBookmark}};
+                const update = { $set: { bookmarks: updatedBookmark } };
                 const newUser = await user.updateOne(update);
-                res.status(201).json({user: newUser});
+                res.status(201).json({ user: newUser });
             } else {
-                res.status(404).json({message: 'user not found'});
+                res.status(404).json({ message: 'user not found' });
             }
-
         }
     }
 }
