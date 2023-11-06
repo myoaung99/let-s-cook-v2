584708114e520e61fd31afa22cb94882dba62528
Myo Myint Aung <myomyintaung064@gmail.com>
2023-11-06 11:15:03 +0630

    temporarily disabled bookmarks page

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link';
-import React, {useState} from 'react';
-import {MenuData} from "@/components/layout/types";
-import {useDispatch} from "@/hooks";
-import {toggleMobileNav} from "@/app/globalSlice";
-import {NextRouter, useRouter} from "next/router";
-import {motion, useMotionValueEvent, useScroll} from 'framer-motion';
-import Image from "next/image";
-import {Playfair} from 'next/font/google'
-import {useAuth, UserButton, useUser} from "@clerk/nextjs";
-import {Button} from "@/components/ui/button";
+import React, { useState } from 'react';
+import { MenuData } from '@/components/layout/types';
+import { useDispatch } from '@/hooks';
+import { toggleMobileNav } from '@/app/globalSlice';
+import { NextRouter, useRouter } from 'next/router';
+import { motion, useMotionValueEvent, useScroll } from 'framer-motion';
+import Image from 'next/image';
+import { Playfair } from 'next/font/google';
+import { useAuth, UserButton, useUser } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
 import {
     Sheet,
     SheetClose,
@@ -18,53 +18,63 @@ import {
     SheetHeader,
     SheetTitle,
     SheetTrigger,
-} from "@/components/ui/sheet"
-import {useClerk} from "@clerk/clerk-react";
+} from '@/components/ui/sheet';
+import { useClerk } from '@clerk/clerk-react';
 
 const SCROLL_WITH_NAV_HEIGHT = 150;
 
 const inter = Playfair({
     subsets: ['latin'],
     display: 'swap',
-})
-
+});
 
 export const Header = () => {
     const [hideNavbar, setHideNavbar] = useState(false);
-    const {scrollY} = useScroll();
+    const { scrollY } = useScroll();
     const dispatch = useDispatch();
 
     useMotionValueEvent(scrollY, 'change', (scrolledPosition) => {
         const previousScrolledPosition = scrollY.getPrevious();
         const isScrollingDown = scrolledPosition > previousScrolledPosition;
         if (isScrollingDown && scrolledPosition > SCROLL_WITH_NAV_HEIGHT) {
-            setHideNavbar(true)
+            setHideNavbar(true);
         } else {
-            setHideNavbar(false)
+            setHideNavbar(false);
         }
-    })
+    });
 
     const toggleMobileMenu = () => {
-        dispatch(toggleMobileNav())
-    }
+        dispatch(toggleMobileNav());
+    };
 
     return (
         <motion.section
-            variants={{hidden: {y: -100}, visible: {y: 0}}}
+            variants={{ hidden: { y: -100 }, visible: { y: 0 } }}
             animate={hideNavbar ? 'hidden' : 'visible'}
-            transition={{delay: 0.3, easings: 'easeInOut'}}
-            className="fixed w-full bg-stone-900 text-white z-10">
+            transition={{ delay: 0.3, easings: 'easeInOut' }}
+            className="fixed w-full bg-stone-900 text-white z-10"
+        >
             <nav className="container h-16 flex items-center justify-between ">
                 <div className="font-bold text-xl px-3 flex gap-2 items-center">
-                    <Image src={"/static/logo.png"} alt={'logo'} width={40} height={40}/>
+                    <Image
+                        src={'/static/logo.png'}
+                        alt={'logo'}
+                        width={40}
+                        height={40}
+                    />
                     <Link href="/">Let's Cook</Link>
                 </div>
-                <DesktopMenuItems/>
+                <DesktopMenuItems />
                 <Sheet>
                     {
-                        <SheetTrigger className='lg:hidden' onClick={toggleMobileMenu}>Open</SheetTrigger>
+                        <SheetTrigger
+                            className="lg:hidden"
+                            onClick={toggleMobileMenu}
+                        >
+                            Open
+                        </SheetTrigger>
                     }
-                    <MobileMenu/>
+                    <MobileMenu />
                 </Sheet>
             </nav>
         </motion.section>
@@ -72,127 +82,141 @@ export const Header = () => {
 };
 
 const MobileMenu = () => {
-    const {signOut} = useClerk();
-    const router = useRouter()
-    const {isLoaded, userId} = useAuth();
-    const {user} = useUser()
-    const imageUrl = user?.imageUrl
+    const { signOut } = useClerk();
+    const router = useRouter();
+    const { isLoaded, userId } = useAuth();
+    const { user } = useUser();
+    const imageUrl = user?.imageUrl;
     return (
         <SheetContent className={'flex flex-col justify-between'}>
             <SheetHeader>
-                <SheetTitle className='text-left'>Let's Cook</SheetTitle>
-                <SheetDescription className='pt-5'>
-                    {
-                        menuData.map((menu) => (
-                            <Link href={menu.href} key={menu.href}>
-                                <SheetClose asChild>
-                                    <p className={`text-lg text-start ${inter.className}`}>{menu.label}</p>
-                                </SheetClose>
-                            </Link>
-                        ))
-                    }
-                    {userId && isLoaded
-                        ? <SheetClose asChild>
-                            <p onClick={() => signOut()} className={`text-lg text-start ${inter.className}`}>Logout</p>
+                <SheetTitle className="text-left">Let's Cook</SheetTitle>
+                <SheetDescription className="pt-5">
+                    {menuData.map((menu) => (
+                        <Link href={menu.href} key={menu.href}>
+                            <SheetClose asChild>
+                                <p
+                                    className={`text-lg text-start ${inter.className}`}
+                                >
+                                    {menu.label}
+                                </p>
+                            </SheetClose>
+                        </Link>
+                    ))}
+                    {userId && isLoaded ? (
+                        <SheetClose asChild>
+                            <p
+                                onClick={() => signOut()}
+                                className={`text-lg text-start ${inter.className}`}
+                            >
+                                Logout
+                            </p>
                         </SheetClose>
-                        : null}
+                    ) : null}
                 </SheetDescription>
             </SheetHeader>
             <SheetFooter>
                 <div>
-                    {userId && isLoaded ?
+                    {userId && isLoaded ? (
                         <SheetClose asChild>
                             <Link href={'/user-profile'}>
-                                {
-                                    imageUrl ? <Image
-                                            className={'rounded-full'}
-                                            width={50} height={50} src={imageUrl}
-                                            alt={user?.fullName ?? 'profile image'}
-                                        />
-                                        : <div className={"w-[50px] h-[50px]"}/>
-                                }
+                                {imageUrl ? (
+                                    <Image
+                                        className={'rounded-full'}
+                                        width={50}
+                                        height={50}
+                                        src={imageUrl}
+                                        alt={user?.fullName ?? 'profile image'}
+                                    />
+                                ) : (
+                                    <div className={'w-[50px] h-[50px]'} />
+                                )}
                             </Link>
                         </SheetClose>
-                        :
-                        <Link href={`/sign-in?redirect_url=${router.asPath}`} className='text-lg'>
+                    ) : (
+                        <Link
+                            href={`/sign-in?redirect_url=${router.asPath}`}
+                            className="text-lg"
+                        >
                             <SheetClose asChild>
-                                <Button className='w-full'>
-                                    Login
-                                </Button>
+                                <Button className="w-full">Login</Button>
                             </SheetClose>
                         </Link>
-                    }
+                    )}
                 </div>
             </SheetFooter>
         </SheetContent>
-    )
-}
+    );
+};
 
 const DesktopMenuItems = () => {
     const router = useRouter();
-    const {isLoaded, userId} = useAuth();
+    const { isLoaded, userId } = useAuth();
 
     const handleToLogin = () => {
-        router.push('/sign-in')
-    }
+        router.push('/sign-in');
+    };
 
     return (
-        <div className={"flex items-center gap-6"}>
+        <div className={'flex items-center gap-6'}>
             <ul className={`hidden menu-list lg:flex items-center gap-1`}>
-                {
-                    menuData.map((menu: MenuData) => (
-                        <li
-                            className={`transition-transform p-2 mx-1 last:me-0 hover:underline ${getActiveTabStyles(menu, router)}
+                {menuData.map((menu: MenuData) => (
+                    <li
+                        className={`transition-transform p-2 mx-1 last:me-0 hover:underline ${getActiveTabStyles(
+                            menu,
+                            router
+                        )}
                             `}
-                            key={menu.href}>
-                            <Link href={menu.href}>
-                                <span className="text-md px-4">{menu.label}</span>
-                            </Link>
-                        </li>
-                    ))
-                }
+                        key={menu.href}
+                    >
+                        <Link href={menu.href}>
+                            <span className="text-md px-4">{menu.label}</span>
+                        </Link>
+                    </li>
+                ))}
             </ul>
 
-            <div className='hidden lg:block'>
-                {!userId || !isLoaded ?
-                    <Button onClick={handleToLogin} variant='ghost'
-                            className={`w-[60px] hover:cursor-pointer text-md`}>
+            <div className="hidden lg:block">
+                {!userId || !isLoaded ? (
+                    <Button
+                        onClick={handleToLogin}
+                        variant="ghost"
+                        className={`w-[60px] hover:cursor-pointer text-md`}
+                    >
                         Login
-                    </Button> :
-                    <UserButton
-                        afterSignOutUrl="/"/>
-
-                }
+                    </Button>
+                ) : (
+                    <UserButton afterSignOutUrl="/" />
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-const getActiveTabStyles = (menu: MenuData, router: NextRouter, activeStyleClasses: string = 'border-2 rounded-sm') => {
+const getActiveTabStyles = (
+    menu: MenuData,
+    router: NextRouter,
+    activeStyleClasses: string = 'border-2 rounded-sm'
+) => {
     const pathName = router.pathname;
     const activeClasses = activeStyleClasses;
     const isRootPage = menu.path === '/';
     if (isRootPage) {
-        return menu.path === pathName ? activeClasses : ''
+        return menu.path === pathName ? activeClasses : '';
     } else {
-        return pathName.startsWith(menu.path) ? activeClasses : ''
+        return pathName.startsWith(menu.path) ? activeClasses : '';
     }
-}
+};
 
 const menuData: Array<MenuData> = [
     {
         href: '/',
         path: '/',
-        label: 'Home'
+        label: 'Home',
     },
     {
         href: '/recipes?filter=Country&value=American',
         path: '/recipes',
-        label: 'Recipes'
+        label: 'Recipes',
     },
-    {
-        href: '/bookmarks',
-        path: '/bookmarks',
-        label: 'Bookmarks'
-    }
-]
+];
